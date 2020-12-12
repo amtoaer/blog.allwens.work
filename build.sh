b17c930bd0b3a6febae42f4c60f228b623999cb9
amtoaer <amtoaer@outlook.com>
2020-12-12 21:05:29 +0800

    配置静态资源压缩

---
+++ build.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+yarn global add gulp
+yarn global add hexo
+yarn
+hexo g
+gulp
---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+var gulp = require('gulp');
+var minifycss = require('gulp-clean-css');
+var uglify = require('gulp-uglify');
+var htmlmin = require('gulp-html-minifier-terser');
+var htmlclean = require('gulp-htmlclean');
+
+// 压缩html
+gulp.task('minify-html', function() {
+    return gulp.src('./public/**/*.html')
+        .pipe(htmlclean())
+        .pipe(htmlmin({
+            collapseWhitespace: true, //从字面意思应该可以看出来，清除空格，压缩html，这一条比较重要，作用比较大，引起的改变压缩量也特别大
+            collapseBooleanAttributes: true, //省略布尔属性的值，比如：<input checked="checked"/>,那么设置这个属性后，就会变成 <input checked/>
+            removeComments: true, //清除html中注释的部分
+            removeEmptyAttributes: true, //清除所有的空属性
+            removeScriptTypeAttributes: true, //清除所有script标签中的type="text/javascript"属性。
+            removeStyleLinkTypeAttributes: true, //清楚所有Link标签上的type属性。
+            minifyJS: true,
+            minifyCSS: true,
+            minifyURLs: true,
+        }))
+        .pipe(gulp.dest('./public'));
+});
+// 压缩css
+gulp.task('minify-css', function() {
+    return gulp.src('./public/**/*.css')
+        .pipe(minifycss({
+            compatibility: 'ie8'
+        }))
+        .pipe(gulp.dest('./public'));
+});
+// 压缩js !代表排除的js,例如['!./public/js/**/*min.js']
+gulp.task('minify-js', function() {
+    return gulp.src(['./public/js/**/.js'])
+        .pipe(uglify()) //压缩混淆
+        .pipe(gulp.dest('./public'));
+});
+
+gulp.task('default',gulp.series(gulp.parallel('minify-html','minify-css','minify-js')));
